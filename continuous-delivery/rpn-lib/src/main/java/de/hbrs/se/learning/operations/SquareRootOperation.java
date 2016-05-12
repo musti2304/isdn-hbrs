7b46c22decdbbf4d8d59c1725b4b375dbd7f2960
haltay2s <hatice.altay@smail.inf.h-brs.de>
2016-05-12 20:35:51 +0200

    quadratwurzel implementiert

---
+++ continuous-delivery/rpn-lib/src/main/java/de/hbrs/se/learning/operations/SquareRootOperation.java
@@ -0,0 +1,10 @@
+package de.hbrs.se.learning.operations;
+
+public class SquareRootOperation extends AbstractUnaryRpnOperation{
+
+	@Override
+	protected double applyOperation(double lhs) {
+		return Math.sin(lhs);
+	}
+
+}
